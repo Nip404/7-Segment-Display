15e7d01cdcbddba73f724e2ccf958e6c69a90e5c
nip403 <41174986+nip403@users.noreply.github.com>
2019-09-25 10:37:32 +0100

    Create counter.py

---
+++ examples/counter.py
@@ -0,0 +1,24 @@
+import engine
+import pygame
+import time
+import sys
+
+def counter():
+    total = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            elif event.type in [pygame.KEYDOWN, pygame.MOUSEBUTTONDOWN]:
+                total += 1
+            
+        engine.Display.update(total)
+
+if __name__ == "__main__":
+    engine.init(2)
+    engine.caption("Example - Counter display")
+
+    counter()
